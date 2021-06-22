4f14d911d78daa0ccdeb2957f6e72795b67bc465
Bernhard Mallinger <bernhard.mallinger@eox.at>
2021-06-22 14:47:52 +0200

    Allow chart helper scripts to bump version and appVersion

--- build_new_chart.sh
+++ build_new_chart.sh
@@ -5,9 +5,9 @@ set -eux -o pipefail
 CHART=$1
 
 NEW_VERSION=$2
-
+NEW_APP_VERSION=$3
 
 sed -i "s/version:.*$/version: ${NEW_VERSION}/" "${CHART}/Chart.yaml"
-sed -i "s/appVersion:.*$/appVersion: ${NEW_VERSION}/" "${CHART}/Chart.yaml"
+sed -i "s/appVersion:.*$/appVersion: ${NEW_APP_VERSION}/" "${CHART}/Chart.yaml"
 
 (cd packages && helm package "../${CHART}")
--- push_new_edc_chart.sh
+++ push_new_edc_chart.sh
@@ -9,5 +9,5 @@ NEW_APP_VERSION=$3
 
 ./build_new_chart.sh "${CHART}" "${NEW_VERSION}" "${NEW_APP_VERSION}"
 
-POD_PROD_CHARTMUSEUM=`kubectl -n prod get pod | grep eox-charts-chartmuseum | awk '{ print $1 }'`
+POD_PROD_CHARTMUSEUM=`kubectl -n prod get pod | grep edc-charts-chartmuseum | awk '{ print $1 }'`
 kubectl cp ./packages/${CHART}-${NEW_VERSION}.tgz prod/${POD_PROD_CHARTMUSEUM}:/storage
