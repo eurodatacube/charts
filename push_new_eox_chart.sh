6d8c662890984697649cbffda430d103f8ec49b0
Bernhard Mallinger <bernhard.mallinger@eox.at>
2021-09-14 11:50:23 +0200

    Modify push scripts to support contributions dir

--- push_new_eox_chart.sh
+++ push_new_eox_chart.sh
@@ -9,5 +9,7 @@ NEW_APP_VERSION=$3
 
 ./build_new_chart.sh "${CHART}" "${NEW_VERSION}" "${NEW_APP_VERSION}"
 
+CHART_BASENAME=`basename $CHART`
+
 POD_PROD_CHARTMUSEUM=`kubectl -n prod get pod | grep eox-charts-chartmuseum | awk '{ print $1 }'`
-kubectl cp ./packages/${CHART}-${NEW_VERSION}.tgz prod/${POD_PROD_CHARTMUSEUM}:/storage
+kubectl cp ./packages/${CHART_BASENAME}-${NEW_VERSION}.tgz prod/${POD_PROD_CHARTMUSEUM}:/storage
